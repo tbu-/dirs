5eed46b847d7885dd66d974c690fd75c7b9b9e24
hch12907 <haochengho12907@gmail.com>
2017-12-07 17:39:14 +0800

    Unix: add bin_home field

--- src/unix.rs
+++ src/unix.rs
@@ -5,6 +5,7 @@ use std::path::{ Path, PathBuf };
 use error::{ Result, DirsError };
 
 pub struct Directories {
+    bin_home: PathBuf,
     cache_home: PathBuf,
     config_home: PathBuf,
     data_home: PathBuf,
@@ -24,9 +25,14 @@ impl Directories {
                 Ok(path)
 
             } else if let Some(home) = env::home_dir() {
-                
-                // Tries to get the HOME path, and push some `path` into it.
                 let mut path = home;
+                
+                // Checks if the home path is empty.
+                if path.as_os_str() == "" { 
+                    return Err(DirsError::HomeMissing); 
+                }
+                
+                // Push some `path` into it if it's not.
                 path.push(fallback_path);
                 path.push(prefix);
                 Ok(path)
@@ -34,19 +40,27 @@ impl Directories {
             } else {
 
                 // At this point, we just give up and return an Err.
-                Err(DirsError::VariableMissing(String::from("$HOME is not set.")))
+                Err(DirsError::HomeMissing)
 
             };
         
-        let cache = make_path("$XDG_CACHE_HOME", ".cache", prefix.as_ref());
-        let config = make_path("$XDG_CONFIG_HOME", ".config", prefix.as_ref());
-        let data = make_path("$XDG_DATA_HOME", ".local/share", prefix.as_ref());
+        // The "=" is not allowed in variable names, so it is guaranteed to be
+        // empty, and therefore, force `make_path` to fallback.
+        let bin = make_path("=", ".local/bin", prefix.as_ref());
+        let cache = make_path("XDG_CACHE_HOME", ".cache", prefix.as_ref());
+        let config = make_path("XDG_CONFIG_HOME", ".config", prefix.as_ref());
+        let data = make_path("XDG_DATA_HOME", ".local/share", prefix.as_ref());
 
         Ok(Directories {
+            bin_home: bin?,
             cache_home: cache?,
             config_home: config?,
             data_home: data?,
         })
+    }
+
+    pub fn bin_home(&self) -> &Path {
+        &self.bin_home
     }
 
     pub fn cache_home(&self) -> &Path {
