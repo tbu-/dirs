ed4645f6655d6f2aea0fcf583674d381acdcb54c
hch12907 <haochengho12907@gmail.com>
2017-12-07 22:39:33 +0800

    Add the result of data_home

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate dirs;
 
 fn main() {
     let dirs = dirs::Directories::with_prefix("foobar", "Foobar").unwrap();
+    println!("data_home:   {:?}", dirs.data_home());
     println!("config_home: {:?}", dirs.config_home());
     println!("cache_home:  {:?}", dirs.cache_home());
     println!("bin_home:    {:?}", dirs.bin_home());
