9b0a29ef31a46ce1fd7dab8804841c4cf0aa53cd
hch12907 <haochengho12907@gmail.com>
2017-12-07 22:00:23 +0800

    Windows: add new field bin_home

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,42 +1,55 @@
 use std::path::Path;
 use std::path::PathBuf;
 
-use Result;
+use error::Result;
 use self::sys::KnownFolder;
 
 mod sys;
 
 pub struct Directories {
+    bin_home: PathBuf,
     cache_home: PathBuf,
     config_home: PathBuf,
-    bin_home: PathBuf,
+    data_home: PathBuf,
 }
 
 impl Directories {
     pub fn with_prefix(_prefix_lowercased: &Path, prefix_capitalized: &Path)
         -> Result<Directories>
     {
-        let get = |k| {
-            let mut buf = try!(sys::known_folder_path(k));
+        let get = |k| -> Result<PathBuf> {
+            let mut buf = sys::get_known_folder_path(k)?;
             buf.push(prefix_capitalized);
             Ok(buf)
         };
-        let cache_home = try!(get(KnownFolder::Temp));
-        let config_home = try!(get(KnownFolder::RoamingAppData));
-        let bin_home = try!(get(KnownFolder::UserProgramFiles));
+
+        let cache_home = get(KnownFolder::Temp)?;
+        let config_home = get(KnownFolder::RoamingAppData)?;
+        let bin_home = get(KnownFolder::UserProgramFiles)?;
+        let data_home = get(KnownFolder::ProgramData)?;
+
         Ok(Directories {
             cache_home: cache_home,
             config_home: config_home,
             bin_home: bin_home,
+            data_home: data_home,
         })
     }
-    pub fn config_home(&self) -> PathBuf {
-        self.config_home.clone()
+
+    
+    pub fn bin_home(&self) -> &Path {
+        &self.bin_home
     }
-    pub fn cache_home(&self) -> PathBuf {
-        self.cache_home.clone()
+
+    pub fn cache_home(&self) -> &Path {
+        &self.cache_home
     }
-    pub fn bin_home(&self) -> PathBuf {
-        self.bin_home.clone()
+
+    pub fn config_home(&self) -> &Path {
+        &self.config_home
+    }
+    
+    pub fn data_home(&self) -> &Path {
+        &self.data_home
     }
 }
