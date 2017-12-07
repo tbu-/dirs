a718a2ff9e5a525b693affb4ce2d99c3dad1733d
hch12907 <haochengho12907@gmail.com>
2017-12-07 23:04:46 +0800

    Give the errors some human-readable messages

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,5 @@
+use std::error::Error as ErrorTrait;
+use std::fmt::{ Display, Formatter, Result as FmtResult };
 use std::result::Result as RustResult;
 use std::string::FromUtf16Error;
 use std::io;
@@ -19,5 +21,35 @@ impl From<FromUtf16Error> for DirsError {
         // So I decided to simply... print an error message and call it a day.
         // <.<
         DirsError::ParseError(String::from("invalid utf-16"))
+    }
+}
+
+use self::DirsError::*;
+impl ErrorTrait for DirsError {
+    fn description(&self) -> &str {
+        match self {
+            &HomeMissing => "unable to obtain the home directory",
+            &IoError(ref err) => err.description(),
+            &ParseError(ref why) => why,
+            &PlatformError(ref why) => why,
+        }
+    }
+
+    fn cause(&self) -> Option<&ErrorTrait> {
+        match self {
+            &IoError(ref why) => Some(why),
+            _ => None
+        }
+    }
+}
+
+impl Display for DirsError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        match self {
+            &HomeMissing => write!(f, "{}", self.description()),
+            &IoError(ref err) => err.fmt(f),
+            &ParseError(ref why) => write!(f, "{}", why),
+            &PlatformError(ref why) => write!(f, "{}", why),
+        }
     }
 }
