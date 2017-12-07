95d16cf17107639c228ae89d51f44a698931e7b3
hch12907 <haochengho12907@gmail.com>
2017-12-07 21:58:37 +0800

    Windows: rewrite the sys code from scratch

--- src/windows/sys.rs
+++ src/windows/sys.rs
@@ -4,39 +4,108 @@ extern crate winapi;
 
 use std::env;
 use std::ffi::OsString;
-use std::fmt;
+use std::os::windows::ffi::OsStringExt;
 use std::path::PathBuf;
-use std::ptr;
-use std::slice;
-use std;
+use self::winapi as api;
 
-use Error;
-use error;
+use error::{ DirsError, Result };
 
-#[allow(non_upper_case_globals)]
-mod uuid {
-    use super::winapi::KNOWNFOLDERID;
-    pub static FOLDERID_RoamingAppData: KNOWNFOLDERID = KNOWNFOLDERID {
-        Data1: 1052149211,
-        Data2: 26105,
-        Data3: 19702,
-        Data4: [160, 58, 227, 239, 101, 114, 159, 61],
-    };
-    pub static FOLDERID_UserProgramFiles: KNOWNFOLDERID = KNOWNFOLDERID {
-        Data1: 1557638882,
-        Data2: 8729,
-        Data3: 19047,
-        Data4: [184, 93, 108, 156, 225, 86, 96, 203],
-    };
-}
-
-#[derive(Clone, Copy, Debug)]
 pub enum KnownFolder {
     RoamingAppData,
     UserProgramFiles,
+    ProgramData,
     Temp,
 }
 
+impl Into<api::GUID> for KnownFolder {
+    fn into(self) -> api::GUID {
+        use self::KnownFolder::*;
+        match self {
+            RoamingAppData => api::GUID {
+                Data1: 1052149211,
+                Data2: 26105,
+                Data3: 19702,
+                Data4: [160, 58, 227, 239, 101, 114, 159, 61],
+            },
+
+            UserProgramFiles => api::GUID {
+                Data1: 1557638882,
+                Data2: 8729,
+                Data3: 19047,
+                Data4: [184, 93, 108, 156, 225, 86, 96, 203],
+            },
+
+            ProgramData => api::GUID {
+                Data1: 1655397762,
+                Data2: 64961,
+                Data3: 19907,
+                Data4: [169, 221, 7, 13, 29, 73, 93, 151],
+            },
+
+            // We leave Temp as all-zeroes, and it won't change any behaviours
+            // even if value is changed anyways, as Temp is a special case in
+            // get_known_folder_path().
+            Temp => api::GUID {
+                Data1: 0,
+                Data2: 0,
+                Data3: 0,
+                Data4: [0, 0, 0, 0, 0, 0, 0, 0],
+            },
+        }
+    }
+}
+
+pub fn get_known_folder_path(folder: KnownFolder) -> Result<PathBuf> {
+    match folder {
+        KnownFolder::Temp => Ok(env::temp_dir()),
+        other => unsafe {
+            // Converts `folder` into KNOWNFOLDERID, which is just an alias
+            // of `GUID`.
+            let rfid: api::KNOWNFOLDERID = Into::into(other);
+            
+            // The magical 0 means NULL here.
+            let mut result = 0 as api::PWSTR;
+
+            // Gets the path.
+            let hresult = shell32::SHGetKnownFolderPath(
+                &rfid as api::REFKNOWNFOLDERID,
+                0u32,
+                0 as api::HANDLE,
+                &mut result
+            );
+
+            if hresult != api::S_OK {
+                return Err(DirsError::PlatformError(
+                    format!("SHGetKnownFolderPath returned {}", hresult)
+                ))
+            }
+
+            // Read the string into a Vec<u16>.
+            // The capacity 130 is chosen because it is the half of MAX_PATH.
+            // (As a note: `const MAX_PATH: usize = 260` )
+            let mut path = Vec::with_capacity(130); {
+            let mut current = result; 
+                while *current != 0 {
+                    path.push(*current);
+                    current = current.offset(1);
+                }
+            };
+            
+            // Convert the u16 to an OsString, and then to a PathBuf.
+            let path = OsString::from_wide(path.as_slice());
+            let path: PathBuf = From::from(path);
+
+            // Drop the WSTR after all of this, and finally return the path.
+            ole32::CoTaskMemFree(
+                result as api::LPVOID
+            );
+
+            Ok(path)
+        },
+    }
+}
+
+/*
 pub struct String {
     value: winapi::PWSTR,
     len: usize,
@@ -128,3 +197,4 @@ pub fn known_folder_path(known_folder: KnownFolder) -> Result<PathBuf, Error> {
         Ok(env::temp_dir())
     })
 }
+*/
