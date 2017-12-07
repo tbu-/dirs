018f1025d86845d4bd847acfe9b2037fc5fa5fbd
hch12907 <haochengho12907@gmail.com>
2017-12-07 18:41:32 +0800

    Extending DirsError with From trait

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,23 @@
+use std::error;
 use std::result::Result as RustResult;
+use std::string::FromUtf16Error;
 use std::io;
 
 /// A specialized `Result` type for this library.
 pub type Result<T> = RustResult<T, DirsError>;
 
+#[derive(Debug)]
 pub enum DirsError {
-    VariableMissing(String),
+    HomeMissing,
+    ParseError(String),
     IoError(io::Error),
+}
+
+impl From<FromUtf16Error> for DirsError {
+    fn from(err: FromUtf16Error) -> Self {
+        // Rust lacks some detailed error messages on Utf16...
+        // So I decided to simply... print an error message and call it a day.
+        // <.<
+        DirsError::ParseError(String::from("invalid utf-16"))
+    }
 }
